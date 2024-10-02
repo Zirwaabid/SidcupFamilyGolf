ae3d8d2176f2481db20ad74376b0ae343b96b2ad
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-02 12:58:13 -0700

    project

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ document.addEventListener("mousemove", function (dets) {
     crsrblur.style.top = dets.y - 250 + "px";
 })
 
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        crsr.style.scale = 4;
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent";
+    });
+    elem.addEventListener("mouseleave", function () {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95C11E"
+        crsr.style.backgroundColor = "#95C11E";
+    });
+})
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.5,
@@ -23,11 +37,62 @@ gsap.to("#nav", {
 gsap.to("#main", {
     backgroundColor: "#000",
     scrollTrigger: {
-        triggar: "#main",
+        trigger: "#main",
         scroller: "body",
         // markers: true,
         start: "top -25%",
         end: "top -70%",
         scrub: 2
-    }
-})
+    },
+});
+
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: "#about-us",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    },
+});
+
+gsap.from(".card", {
+    scale: 0.8,
+    // opacity: 0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+        trigger: ".card",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    },
+});
+
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+});
+
+gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+});
